97bbe44176086e44ae5acb47726a06bf588766e0
heumsi <heumsi@naver.com>
2023-01-26 16:51:02 +0900

    fix: fix wrong api_url

--- pcconfig.py
+++ pcconfig.py
@@ -5,6 +5,6 @@ import pynecone as pc
 config = pc.Config(
     app_name="hollys",
     port=os.getenv("PORT", "3000"),
-    api_url=os.getenv("API_URL", "192.168.1.1:8000"),
+    api_url=os.getenv("API_URL", "ws:localhost:8000"),
     db_url=os.getenv("DB_URL", "sqlite:///pynecone.db"),
 )
